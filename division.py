5822b02c6f45fe9f986881bb24ceece067884444
Sathana Dhanalakshmi C <sathanadhanalakshmic_bec28@mepcoeng.ac.in>
2026-04-14 19:32:05 +0530

    Add files via upload

---
+++ division.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    a = int(input("Enter a number:"))
+    b = int(input("Enter a number:"))
+    print("Floor division of a and b:",a//b)
+    print("Division of a and b:",a/b)
