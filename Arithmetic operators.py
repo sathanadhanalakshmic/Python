cccb0ebe5b69d24a4dd6e066f7df61c50b6aef50
Sathana Dhanalakshmi C <sathanadhanalakshmic_bec28@mepcoeng.ac.in>
2026-04-14 19:18:19 +0530

    Add files via upload

---
+++ Arithmetic operators.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input("Enter a number:"))
+    b = int(input("Enter a number:"))
+    print("Addition:",a+b)
+    print("Subtraction:",a-b)
+    print("Multiplication:",a*b)
