1f7d2986eec10c6fa6cc0cb6873b9682b09b7f86
Sathana Dhanalakshmi C <sathanadhanalakshmic_bec28@mepcoeng.ac.in>
2026-04-15 10:39:32 +0530

    Add files via upload

---
+++ Check a number is odd or even.py
@@ -0,0 +1,7 @@
+#Python program to check a number is odd or even #
+
+num=int(input("Enter a number:"))
+if(num%2==0):
+   print("The number is a even number")
+else:
+   print("The number is a odd number")
