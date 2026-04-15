c43aaaadf4180822168fef8d02e66a6591bdbe22
Sathana Dhanalakshmi C <sathanadhanalakshmic_bec28@mepcoeng.ac.in>
2026-04-15 10:26:58 +0530

    Add files via upload

---
+++ Read the weight of an object in Kg and print its weight in pound and tonne.py
@@ -0,0 +1,6 @@
+#Python program to read the weight of an object in Kg and print its weight in pound and tonne#
+a=int(input("Enter the weight in Kg:"))
+p=a*2.20
+t=a*0.001
+print("Weight in pound:",p)
+print("Weight in  tonne:",t)
