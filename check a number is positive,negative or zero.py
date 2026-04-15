9e37a867cd73622c7aff4423b3be72e7a18a548b
Sathana Dhanalakshmi C <sathanadhanalakshmic_bec28@mepcoeng.ac.in>
2026-04-15 10:38:01 +0530

    Add files via upload

---
+++ check a number is positive,negative or zero.py
@@ -0,0 +1,8 @@
+#Python program to check a number is positive ,negative or zero #
+num=int(input("Enter a number:"))
+if(num>0):
+   print("The number is a positive number")
+elif(num<0):
+   print("The number is a negative number")
+else:
+   print("The number is zero")
