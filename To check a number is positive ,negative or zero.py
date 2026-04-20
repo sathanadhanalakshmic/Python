89091d7604eb51b3af0b4f3c3d7ecb42c739b8f8
Sathana Dhanalakshmi C <sathanadhanalakshmic_bec28@mepcoeng.ac.in>
2026-04-20 11:40:08 +0530

    Add files via upload

---
+++ To check a number is positive ,negative or zero.py
@@ -0,0 +1,7 @@
+num=int(input("Enter a number:"))
+if(num>0):
+   print("The number is a positive number")
+elif(num<0):
+   print("The number is a negative number")
+else:
+   print("The number is zero")
